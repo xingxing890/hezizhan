b69ca9366a8c8893b554e93cb8a61d78f7b977fe
xingxing890 <929235241@qq.com>
2025-08-14 16:49:11 +0800

    Add files via upload
    Signed-off-by: xingxing890 <929235241@qq.com>

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import NavHomeView from '../views/NavHomeView.vue'
+import TestView from '../views/TestView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: NavHomeView,
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/AdminView.vue'),
+      meta: {
+        title: '管理后台 - 猫猫导航',
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: TestView,
+      meta: {
+        title: '环境变量测试 - 猫猫导航'
+      }
+    },
+  ],
+})
+
+// 路由前置守卫
+router.beforeEach((to, from, next) => {
+  // 设置页面标题
+  if (to.meta?.title) {
+    document.title = to.meta.title
+  } else {
+    document.title = '猫猫导航'
+  }
+
+  next()
+})
+
+export default router
